6a053fe20f8933398f3ffcec94064fd1accacf58
Juan David Morales <moralesjud@globalhitss.com>
2025-04-11 06:32:20 -0500

    omitiendo archivos

--- PruebaTecnica/Aplication/VirtualMachines/V1/Queries/GetAllVirtualMachinesQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-using PruebaTecnica.Aplication.VirtualMachines.V1.DTOs;
-using PruebaTecnica.Domain.Wrappers;
-using System.Collections.Generic;
-
-namespace PruebaTecnica.Aplication.VirtualMachines.V1.Queries
-{
-    public record struct GetAllVirtualMachinesQuery : IRequest<Response<List<GetAllVirtualMachinesResponse>>>;
-}
